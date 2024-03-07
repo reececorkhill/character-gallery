c2d286c83ee1e842f69711ced9a4f95d15e40503
Reece <reece@smartit-webdevelopment.com>
2024-03-07 21:10:25 +0000

    Added test h1.

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,28 +1,33 @@
 const Character = ({ name, imgUrl, birth, death, race, realm, spouse }) => {
     return (
-        <div>
-            <h2>{ name }</h2>
+        <>
+            <div>
+                <h1>New Test</h1>
+            </div>
+            <div>
+                <h2>{ name }</h2>
 
-            <img src={ imgUrl } alt={ name } />
+                <img src={ imgUrl } alt={ name } />
 
-            <ul>
-                <li>
-                    Date of Birth: { birth }
-                </li>
-                <li>
-                    Date of Death: { death }
-                </li>
-                <li>
-                    Race: { race }
-                </li>
-                <li>
-                    Realm: { realm }
-                </li>
-                <li>
-                    Spouse: { spouse }
-                </li>
-            </ul>
-        </div>
+                <ul>
+                    <li>
+                        Date of Birth: { birth }
+                    </li>
+                    <li>
+                        Date of Death: { death }
+                    </li>
+                    <li>
+                        Race: { race }
+                    </li>
+                    <li>
+                        Realm: { realm }
+                    </li>
+                    <li>
+                        Spouse: { spouse }
+                    </li>
+                </ul>
+            </div>
+        </>
     );
 };
 
